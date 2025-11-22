9aa79a7002961cdeb837fd439e91a5fe3f7ae0d2
ArianaPolar-dev <polar3dev@gmail.com>
2025-11-21 21:02:21 -0400

    Refactor Vite config to use defineConfig

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,6 @@
-export default {
-  base: "/dashboard-interactivo/",  // Pon el nombre exacto de tu repo aquí
-  plugins: [react()],
-}
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
 export default defineConfig({
+  base: "/dashboard-interactivo/",
   plugins: [react()],
 })
